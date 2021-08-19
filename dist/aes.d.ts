226dd1c3f9ac46018db0b078b29881971e76fb4f
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2021-08-20 02:22:43 +0530

    Fixed build line endings and upgraded dependencies

--- dist/aes.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="node" />
-export declare const aesEncrypt: (encryption_key: string, encryption_mac: string, data: Buffer) => Promise<string>;
-export declare const aesDecrypt: (encryption_key: string, encryption_mac: string, encrypted_data: string) => Promise<{
-    isVerified: boolean;
-    data: Buffer;
-}>;
